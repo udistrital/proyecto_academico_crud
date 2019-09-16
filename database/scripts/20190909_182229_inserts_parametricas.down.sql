4b5a1fe3510a7d9e01d414f58b102a78f04cbd75
JuanPiedrahita <juangonzalez1597@gmail.com>
2019-09-16 16:36:20 -0500

    #4 inserts parametricas

---
+++ database/scripts/20190909_182229_inserts_parametricas.down.sql
@@ -0,0 +1,5 @@
+delete from proyecto_academico.tipo_titulación;
+delete from proyecto_academico.tipo_registro;
+delete from proyecto_academico.enfasis;
+delete from proyecto_academico.nivel_formacion;
+delete from proyecto_academico.metodologia;
