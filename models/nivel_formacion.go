34b55bc1a5c8aa4b9523bb2e3750146beb2a1959
CamiloQui <camiloquirogam@outlook.com>
2021-01-06 10:58:22 -0500

    feat/arreglor de error getall

--- models/nivel_formacion.go
+++ models/nivel_formacion.go
@@ -18,7 +18,7 @@ type NivelFormacion struct {
 	NumeroOrden       float64   `orm:"column(numero_orden);null"`
 	FechaCreacion     string    `orm:"column(fecha_creacion);type(timestamp without time zone)"`
 	FechaModificacion string    `orm:"column(fecha_modificacion);type(timestamp without time zone)"`
-	NivelFormacionPadreId *NivelFormacion `orm:"column(nivel_formacion_padre_id);rel(fk)"`
+	NivelFormacionPadreId *NivelFormacion `orm:"column(nivel_formacion_padre_id);rel(fk);null"`
 }
 
 func (t *NivelFormacion) TableName() string {
@@ -64,6 +64,7 @@ func GetAllNivelFormacion(query map[string]string, fields []string, sortby []str
 			qs = qs.Filter(k, v)
 		}
 	}
+
 	// order by:
 	var sortFields []string
 	if len(sortby) != 0 {
@@ -121,6 +122,7 @@ func GetAllNivelFormacion(query map[string]string, fields []string, sortby []str
 				ml = append(ml, m)
 			}
 		}
+
 		return ml, nil
 	}
 	return nil, err
