f95ffaa835b380b464d96f384e1e3eede9bb6211
Fabian Barreto <fabibeto11@hotmail.com>
2021-01-06 10:49:42 -0500

    hotfix:se agrega al modelo parametro null a nuevo campo para ajustar la carga

--- models/nivel_formacion.go
+++ models/nivel_formacion.go
@@ -10,15 +10,15 @@ import (
 )
 
 type NivelFormacion struct {
-	Id                int       `orm:"column(id);pk;auto"`
-	Nombre            string    `orm:"column(nombre)"`
-	Descripcion       string    `orm:"column(descripcion);null"`
-	CodigoAbreviacion string    `orm:"column(codigo_abreviacion);null"`
-	Activo            bool      `orm:"column(activo)"`
-	NumeroOrden       float64   `orm:"column(numero_orden);null"`
-	FechaCreacion     string    `orm:"column(fecha_creacion);type(timestamp without time zone)"`
-	FechaModificacion string    `orm:"column(fecha_modificacion);type(timestamp without time zone)"`
-	NivelFormacionPadreId *NivelFormacion `orm:"column(nivel_formacion_padre_id);rel(fk)"`
+	Id                    int             `orm:"column(id);pk;auto"`
+	Nombre                string          `orm:"column(nombre)"`
+	Descripcion           string          `orm:"column(descripcion);null"`
+	CodigoAbreviacion     string          `orm:"column(codigo_abreviacion);null"`
+	Activo                bool            `orm:"column(activo)"`
+	NumeroOrden           float64         `orm:"column(numero_orden);null"`
+	FechaCreacion         string          `orm:"column(fecha_creacion);type(timestamp without time zone)"`
+	FechaModificacion     string          `orm:"column(fecha_modificacion);type(timestamp without time zone)"`
+	NivelFormacionPadreId *NivelFormacion `orm:"column(nivel_formacion_padre_id);rel(fk);null"`
 }
 
 func (t *NivelFormacion) TableName() string {
